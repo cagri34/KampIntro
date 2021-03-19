7ea8964bb62db5cb0dc45325d8a9fd022ff2fc67
cagricivelek <cagricivelek@outlook.com>
2021-03-19 18:09:10 +0300

    Devam

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] isimler = new string[] { "Engin", "Murat", "Kerem", "Halil" };
+
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine(isimler[1]);
+            Console.WriteLine(isimler[2]);
+            Console.WriteLine(isimler[3]);
+        }
+    }
+}
