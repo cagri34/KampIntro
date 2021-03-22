97c1efea62b802af9e39f47982048807425cf496
cagricivelek <cagricivelek@outlook.com>
2021-03-22 17:45:37 +0300

    22 Mart 2021

---
+++ OOP2/MusteriManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    // Businnes Classes
+
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+
+        }
+    }
+}
