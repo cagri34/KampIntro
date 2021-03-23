dfe4c49dc35e0de8ebf5292b961c664c289c36ec
cagricivelek <cagricivelek@outlook.com>
2021-03-23 15:34:30 +0300

    23 Mart 2021

---
+++ OOP3/KrediManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    interface IKrediManager
+    {
+        // Birbirinin alternatifi olan fakat kod içerikleri farklı olan durumlar için kullanılır.
+
+        void Hesapla();
+        void BiseyYap();
+    }
+}
