d5ec393add0d1c5e22f8a3ac6b0640a302e468e5
cagricivelek <cagricivelek@outlook.com>
2021-03-29 11:00:54 +0300

    29 Mart 2021

--- ClassMethodDemo/Program.cs
+++ ClassMethodDemo/Program.cs
@@ -36,7 +36,7 @@ namespace ClassMethodDemo
             {
                 customerManager.Sil(customer);
             }
-            
+           
         }
     }
 }
---
+++ GameProject/GamerManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    // MicroService
+    class GamerManager : IGamerService
+    {
+        IUserValidationService _userValidationService;
+
+        public GamerManager(IUserValidationService userValidationService)
+        {
+           _userValidationService = userValidationService;
+        }
+
+        public void Add(Gamer gamer)
+        {
+            if (_userValidationService.Validate(gamer))
+            {
+                Console.WriteLine("Kayıt oldu.");
+            }
+            else
+            {
+                Console.WriteLine("Doğrulama başarısız. Kayıt başarısız.");
+            }
+        }
+
+        public void Delete(Gamer gamer)
+        {
+            Console.WriteLine("Kayıt Güncellendi.");
+        }
+
+        public void Update(Gamer gamer)
+        {
+            Console.WriteLine("Kayıt Silindi.");
+        }
+    }
+}
